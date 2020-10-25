e6f94a20e78d62b6a643958085310c3e062fc952
Aney <aney@github.com>
2020-10-25 16:37:11 +0000

    Changes to install script to make slightly more minimal

--- install.sh
+++ install.sh
@@ -8,27 +8,41 @@ apt install sudo xorg -y
 apt update && upgrade -y
 
 # Terminal shit
-apt install git zsh tmux vim neofetch wget curl gdebi parted -y
+apt install git zsh tmux vim neofetch wget -y
 
 # Image viewer, music player, rss feed, etc
-apt install feh cmus newsboat youtube-dl -y
+apt install cmus newsboat youtube-dl -y
 
-# PDF Reader
-apt install zathura
+# Image viewer, PDF Reader (GUI)
+apt install feh zathura -y
 
 # Network Tools
 #apt install net-tools wicd -y
 
 # Audio
-apt install alsa-utils alsa-oss pulseaudio pavucontrol -y
+apt install alsa-utils alsa-oss pulseaudio -y
+
+# Pulseaudio GUI
+apt install pavucontrol -y 
 
 # Random shit
 apt install gtypist -y
 
 # Torrenting 
 apt install transmission-daemon transmission-cli transmission-remote-cli -y
-
 # Setup the user for transmission-daemon
+
+# lf file manager
+mkdir ~/.local/bin
+# Add to path for this session
+export PATH=~/.local/bin:$PATH
+# Add to bash, this will be switched to zsh when I figure that out 
+echo "export PATH=~/.local/bin:\$PATH" >> .bashrc
+# Download a pre-built binary
+curl -L https://github.com/gokcehan/lf/releases/latest/download/lf-linux-amd64.tar.gz | tar xzC ~/.local/bin
+
+
+# GUI
 
 # brave-browser
 # https://brave-browser.readthedocs.io/en/latest/installing-brave.html#linux
@@ -43,14 +57,6 @@ apt update
 
 apt install brave-browser -y
 
-# lf file manager
-mkdir ~/.local/bin
-# Add to path for this session
-export PATH=~/.local/bin:$PATH
-# Add to bash, this will be switched to zsh when I figure that out 
-echo "export PATH=~/.local/bin:\$PATH" >> .bashrc
-# Download a pre-built binary
-curl -L https://github.com/gokcehan/lf/releases/latest/download/lf-linux-amd64.tar.gz | tar xzC ~/.local/bin
 
 # Window Manager
 
---
+++ test_install.md
@@ -0,0 +1,31 @@
+# Test Install
+
+Using a VM and other thinkpad to decide what I can remove/add.
+
+The idea is to get full functionality, but also be minimal and lightweight.
+
+# OS
+
+Debian, minimal net install. 
+
+Only select `standard system utilities` or whatever it's called.
+
+This can be skipped and stuff installed as needed also
+
+# non-free
+
+- firmware-iwlwifi
+	- For thinkpad wifi card
+
+# Errything else
+
+- sudo
+	- `su -`, `adduser <USER> sudo`, then relog as the user.
+- tmux
+- htop
+- neofetch
+- git
+- wget 
+- build-essential
+- zsh
+- oh-my-zsh
