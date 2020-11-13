f2f7b3c168a852f0e1f869bb3ce5f4dc3f194e6e
Nathan Steel <nathan@x220.lan>
2020-11-13 11:41:29 +0000

    Additions to install script

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ apt install cmus newsboat youtube-dl mpv scrot -y
 # Image viewer, PDF Reader (GUI)
 apt install feh zathura -y
 
-# Hewwo
+# Xanmod Kernel
 echo 'deb http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-kernel.list && wget -qO - https://dl.xanmod.org/gpg.key | sudo apt-key add -
 apt update && apt install linux-xanmod -y
 
@@ -39,11 +39,17 @@ apt install gtypist -y
 apt install transmission-daemon transmission-cli transmission-remote-cli -y
 
 #systemctl stop transmission-daemon.service
-#/lib/systemd/system/tranmission-daemon.service # Change username to user
+#/lib/systemd/system/tranmission-daemon.service # Change debian-transmission to $USER
 # systemctl daemon-reload
-# start service
+# start/restart service
 
 # Setup the user for transmission-daemon
+
+# Virtualisation
+apt install qemu-kvm libvirt-clients libvirt-daemon-system virt-manager
+# Add to virtualisation groups
+adduser nathan libvirt
+adduser nathan libvirt-qemu
 
 # brave-browser
 # https://brave-browser.readthedocs.io/en/latest/installing-brave.html#linux
@@ -60,12 +66,27 @@ apt install libx11-dev libxft-dev libxinerama-dev -y
 # User
 su nathan 
 
-# dwm Window manager
-mkdir ~/git
-cd ~/git
-git clone https://git.suckless.org/dwm
-cd dwm
-make clean install
+# Create directories
+# Need to modularise the if [ -d ] then add these with it
+mkdir ~/downloads/ ~/pictures/ ~/video/ ~/recordings/ ~/documents/ ~/music/
+#mkdir /mnt/usb/ /mnt/hdd/ /mnt/network/
+
+
+# Oh my Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Set location to download source code and other repos
+if [! -d "~/agit/" ]; then
+	mkdir ~/agit/
+fi
+
+# Suckless installs
+# Not sure how to do the intsall without sudo prompt...
+# dwm
+cd ~/agit/
+git clone https://git.suckless.org/dwm ~/agit/adwm
+cd ~/agit/adwm/
+#sudo make clean install
 
 # st terminal
 
@@ -76,12 +97,21 @@ make clean install
 
 # Setup the bin directory for custom scripts
 mkdir ~/.local/bin
-# Vi mode terminal and local bin
+# Vi mode terminal and local bin # This also happens in dotfiles, so kinda redundant
 echo "set -o vi\nexport PATH=~/.local/bin:\$PATH" >> .zshrc
 echo "export PATH=~/.local/bin/*:\$PATH" >> .zshrc
 # Add to path for this session
-export PATH=~/.local/bin:$PATH
+export PATH=~/.local/bin/:$PATH
 export PATH=~/.local/bin/*:$PATH
+
+# Muh scripties
+cd ~/agit/
+git clone https://github.com/aney/scripts ~/agit/ascripts/
+cd ~/agit/ascripts/
+# create a sym link. Will change to cp if others start using my install.sh
+ln -s $(pwd)/dmenu/ ~/.local/bin/
+ln -s $(pwd)/config/ ~/.local/bin/
+ln -s $(pwd)/backup/ ~/.local/bin/
 
 # lf file manager
 # Download a pre-built binary
@@ -89,3 +119,13 @@ curl -L https://github.com/gokcehan/lf/releases/latest/download/lf-linux-amd64.t
 
 # Change default shell to zsh
 chsh -s /bin/zsh
+
+# dotfiles
+# This will overwrite existing dotfiles...
+cd ~/agit/
+git clone https://github.com/aney/dotfiles ~/agit/adotfiles/
+cd ~/agit/adotfiles/
+cp -r * ~/.
+
+# Extras. Bg image, icons, etc.
+
